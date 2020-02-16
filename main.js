4470e7e03a3531212be49cb90a435b82c5eb5cb5
sorz.code <germain717@gmail.com>
2020-02-17 00:56:17 +0800

    performance optimization
    remove run at document idle to run faster

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 // ==UserScript==
 // @name         Youtube Ads auto reload
 // @namespace    http://tampermonkey.net/
-// @version      2.0
+// @version      2.1
 // @description  Reload youtube page when adblocker can't block youtube ads
 // @author       sorz.code
 // @match        *://*.youtube.com/*
 // @grant        none
 // @run-at       document-body
-// @run-at       document-idle
 // @require http://code.jquery.com/jquery-3.4.1.min.js
 
 // ==/UserScript==
@@ -69,49 +68,6 @@
             check();
             noRefreshCheckInject();
             $(document).off('DOMNodeInserted', 'ytd-player');
-        });
-    }
-
-    fullRefreshCheck();
-
-})();    }
-
-    function noRefreshCheckInject() {
-        // Select the node that will be observed for mutations
-        const targetNode = document.getElementsByClassName('video-stream html5-main-video')[0];
-
-        // Options for the observer (which mutations to observe)
-        const config = {
-            attributes: true
-        };
-
-        // Callback function to execute when mutations are observed
-        const callback = function(mutationsList, observer) {
-            // Use traditional 'for loops' for IE 11
-            for (let mutation of mutationsList) {
-                if (mutation.attributeName === 'src') {
-                    console.log(mutation)
-                    //console.log('no-refresh page', window.location.href);
-                    check();
-                }
-            }
-        };
-
-        // Create an observer instance linked to the callback function
-        const observer = new MutationObserver(callback);
-
-        // Start observing the target node for configured mutations
-        observer.observe(targetNode, config);
-    }
-
-    function fullRefreshCheck() {
-        var flag = true;
-        $(document).on('DOMNodeInserted','ytd-player', function(e) {
-            if(flag){
-                check();
-                noRefreshCheckInject();
-                flag = false;
-            }
         });
     }
 
